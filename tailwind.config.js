9f04697d01bc179605b19101a6f41e00519b6126
Ben Horowitz <bhorowit@gmail.com>
2025-04-15 13:05:40 -0700

    First draft of news tab

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       colors: {
         primary: '#5865F2',   // Blurple, Discord's brand color
         secondary: '#00BFFF', // Electric cyan
+        secondary_dark: '#089182',
         accent: '#8B5CF6',    // Vibrant purple
         neutral: '#C9C9C9',   // Gray
         dark: '#202020',      // Dark Gray
---
+++ tests/test_news.py
@@ -0,0 +1,4 @@
+def test_index(client):
+    response = client.get('/news')
+    assert response.status_code == 200
+    assert b'<h1>News and Announcements</h1>' in response.data
