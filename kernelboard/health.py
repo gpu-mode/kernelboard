be4069283a4979ac356d09164db99f081cb10386
Ben Horowitz <bhorowit@gmail.com>
2025-04-23 18:16:18 -0700

    Revert "Revert "Initial setup of Redis""
    This reverts commit 7d30be8d325b24539f0d84b92915b78cd02f5ea5.

--- kernelboard/health.py
+++ kernelboard/health.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, current_app as app
 from .db import get_db_connection
+import redis
 
 
 blueprint = Blueprint('health', __name__, url_prefix='/health')
@@ -9,12 +10,25 @@ blueprint = Blueprint('health', __name__, url_prefix='/health')
 def health():
     all_checks_passed = True
 
-    conn = get_db_connection()
     try:
-        conn.cursor().execute("SELECT 1")
-    except:
-        app.logger.error("Database health check failed")
+        db_conn = get_db_connection()
+        with db_conn.cursor() as cur:
+            cur.execute("SELECT 1")
+    except Exception as e:
+        app.logger.error(f"Database health check failed: {e}")
         all_checks_passed = False
+
+    redis_url = app.config['REDIS_URL']
+    if not redis_url:
+        app.logger.error("REDIS_URL is not set in the application configuration.")
+        all_checks_passed = False
+    else:
+        try:
+            redis_conn = redis.from_url(redis_url)
+            redis_conn.ping()
+        except Exception as e:
+            app.logger.error(f"Redis health check failed: {e}")
+            all_checks_passed = False
 
     if all_checks_passed:
         return {
