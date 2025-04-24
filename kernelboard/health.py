a0684c6b4cda7a281462e05906b1dc2163418b9c
Ben Horowitz <bhorowit@gmail.com>
2025-04-23 18:23:01 -0700

    Use Heroku incantation for Redis connection

--- kernelboard/health.py
+++ kernelboard/health.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, current_app as app
 from .db import get_db_connection
+from urllib.parse import urlparse
 import redis
 
 
@@ -18,13 +19,18 @@ def health():
         app.logger.error(f"Database health check failed: {e}")
         all_checks_passed = False
 
-    redis_url = app.config['REDIS_URL']
+    redis_url = urlparse(app.config['REDIS_URL'])
     if not redis_url:
         app.logger.error("REDIS_URL is not set in the application configuration.")
         all_checks_passed = False
     else:
         try:
-            redis_conn = redis.from_url(redis_url)
+            redis_conn = redis.Redis(
+                host=redis_url.hostname,
+                port=redis_url.port,
+                password=redis_url.password,
+                ssl=(redis_url.scheme == "rediss"),
+                ssl_cert_reqs=None)
             redis_conn.ping()
         except Exception as e:
             app.logger.error(f"Redis health check failed: {e}")
