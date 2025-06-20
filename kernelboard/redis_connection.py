b4e1c5c915c05f9fe933b55d7b94beb3d46d677a
Yang Wang <elainewy@meta.com>
2025-06-20 11:17:17 -0700

    setup link
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- kernelboard/redis_connection.py
+++ kernelboard/redis_connection.py
@@ -1,17 +1,24 @@
 import os
 import redis
 
+import os
+import redis
 
-def create_redis_connection(cert_reqs: str | None = None) -> redis.Redis | None:
-    """
-    Creates a redis connection using application configuration.
-    """
-    url = os.getenv('REDIS_URL')
-    if url is None:
-        return None
+_redis_client = None
 
-    kwargs = {}
-    if cert_reqs:
-        kwargs['ssl_cert_reqs'] = cert_reqs
-    
-    return redis.from_url(url, **kwargs)
+def get_redis_connection(cert_reqs: str | None = None) -> redis.Redis | None:
+    """
+    Retrieves or initializes a singleton redis connection.
+    """
+    global _redis_client
+    if _redis_client is None:
+        url = os.getenv('REDIS_URL')
+        if url is None:
+            return None
+
+        kwargs = {}
+        if cert_reqs and url.startswith('rediss://'):
+             kwargs['ssl_cert_reqs'] = cert_reqs
+
+        _redis_client = redis.from_url(url, **kwargs)
+    return _redis_client
