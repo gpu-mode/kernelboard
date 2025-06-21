27fd949db696293ea06c29923b159b71f168cfcc
Yang Wang <elainewy@meta.com>
2025-06-20 23:29:32 -0700

    setup link
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,6 +1,6 @@
-from kernelboard.redis_connection import get_redis_connection
+from kernelboard.redis_connection import create_redis_connection
 
 def test_get_and_close_redis_connection(app):
     with app.app_context():
-        conn = get_redis_connection()
+        conn = create_redis_connection()
         assert conn is not None
