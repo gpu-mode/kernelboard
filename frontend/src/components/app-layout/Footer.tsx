9bad20d945f4ec8c5b32e048c25f9284cd60ef89
Yang Wang <elainewy@meta.com>
2025-07-08 15:49:12 -0700

    setup link
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- frontend/src/components/app-layout/Footer.tsx
+++ frontend/src/components/app-layout/Footer.tsx
@@ -1,13 +1,22 @@
-import { Box, Container, Link, Typography } from "@mui/material";
+import { Box, Container, Link, styled, Typography } from "@mui/material";
 
-const links = [
-  { label: "Discord", href: "https://discord.gg/gpumode" },
-  { label: "X", href: "https://x.com/GPU_MODE" },
-  { label: "YouTube", href: "https://www.youtube.com/@GPUMODE" },
-  { label: "GitHub", href: "https://github.com/gpu-mode/" },
-];
+export const FooterLinkContainer = styled(Container)(({ theme }) => ({
+    maxWidth: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    gap: theme.spacing(2),
+    alignItems: 'center',
+  }));
 
 export default function Footer() {
+ const links = [
+    { label: "Discord", href: "https://discord.gg/gpumode" },
+    { label: "X", href: "https://x.com/GPU_MODE" },
+    { label: "YouTube", href: "https://www.youtube.com/@GPUMODE" },
+    { label: "GitHub", href: "https://github.com/gpu-mode/" },
+  ];
+
   return (
     <Box
       component="footer"
@@ -17,16 +26,7 @@ export default function Footer() {
         textAlign: "center",
       }}
     >
-      <Container
-        sx={{
-          maxWidth: "100%",
-          display: "flex",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          gap: 2,
-          alignItems: "center",
-        }}
-      >
+      <FooterLinkContainer>
         {links.map(({ label, href }) => (
           <Link
             key={label}
@@ -40,7 +40,7 @@ export default function Footer() {
         <Typography variant="body2" color="text.secondary" sx={{ ml: 2 }}>
           © 2025 GPU MODE
         </Typography>
-      </Container>
+      </FooterLinkContainer>
     </Box>
   );
 }
