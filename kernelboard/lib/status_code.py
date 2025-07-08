960ef91d7a21a90bb60647e62d68b19e98170ac3
Yang Wang <elainewy@meta.com>
2025-07-08 12:39:08 -0700

    setup link
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- kernelboard/lib/status_code.py
+++ kernelboard/lib/status_code.py
@@ -1,35 +1,22 @@
-from enum import IntEnum
 from flask import jsonify
-
-# kernelboard/utils/response.py
-from flask import jsonify
-from enum import IntEnum
+from http import HTTPStatus
 
 
-class HttpStatusCode(IntEnum):
-    OK = 200
-    BAD_REQUEST = 400
-    UNAUTHORIZED = 401
-    FORBIDDEN = 403
-    NOT_FOUND = 404
-    CONFLICT = 409
-    INTERNAL_SERVER_ERROR = 500
 
-
-def make_response(data=None, message="Success", code=0, status_code=HttpStatusCode.OK):
+def make_response(data=None, message="Success", code=0, status_code=HTTPStatus.OK):
     return jsonify({"code": code, "message": message, "data": data}), int(status_code)
 
 
-def httpSuccess(data=None, message="Success"):
+def http_success(data=None, message="Success"):
     return make_response(
-        data=data, message=message, code=0, status_code=HttpStatusCode.OK
+        data=data, message=message, code=0, status_code=HTTPStatus.OK
     )
 
 
-def httpError(
+def http_error(
     message="Error",
     code=10000,
-    status_code=HttpStatusCode.INTERNAL_SERVER_ERROR,
+    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
     data=None,
 ):
     return make_response(data=data, message=message, code=code, status_code=status_code)
