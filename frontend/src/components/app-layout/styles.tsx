159f262ebbf01aae70689efb468dd57f047d1043
Yang Wang <elainewy@meta.com>
2025-07-08 21:05:19 -0700

    setup link
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- frontend/src/components/app-layout/styles.tsx
+++ frontend/src/components/app-layout/styles.tsx
@@ -1,4 +1,12 @@
+import { flexRowCenter, mediumText } from "../common/styles/shared_style";
+
 export const flashIconStyle = {
   color: "#FFD700",
   fontSize: 20,
 };
+
+export const brandStyle = {
+    ...flexRowCenter,
+    ...mediumText,
+    fontWeight: "bold"
+}
