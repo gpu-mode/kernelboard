8d1372c6073a8bd1fa444f646e4ae75c1ef70fe6
Yang Wang <elainewy@meta.com>
2025-08-22 10:59:52 -0700

    add web api

---
+++ src/migrations/20250821_01_TlQxS-website-submission.py
@@ -0,0 +1,16 @@
+"""
+website_submission
+"""
+
+from yoyo import step
+
+__depends__ = {'20250728_01_Q3jso-fix-code-table'}
+
+steps = [
+    step(
+        """
+        ALTER TABLE leaderboard.user_info
+        ADD COLUMN IF NOT EXISTS web_auth_id VARCHAR(255) DEFAULT NULL;
+        """
+    )
+]
