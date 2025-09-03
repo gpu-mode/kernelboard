bce8bdc20c80e8f6107867952f4ee040612e2199
Yang Wang <elainewy@meta.com>
2025-09-03 12:47:34 -0700

    addid

---
+++ frontend/src/lib/types/exit_code.ts
@@ -0,0 +1,12 @@
+const exitCodeMessages: { [key: number]: string } = {
+  0: "Program completed successfully.",
+  110: "A CUDA API call failed.",
+  111: "Could not setup file descriptor for custom pipe.",
+  112: "The run did not crash, but tests failed.",
+  113: "Problem parsing test/benchmark specification.",
+  114: "Process was shut down because it timed out.",
+};
+
+export function getExitCodeMessage(code: number): string {
+  return exitCodeMessages[code] ?? `Unknown exit code: ${code}`;
+}
