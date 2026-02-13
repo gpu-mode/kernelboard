7baa5d6d53751837c5fd5e30356f5ce609d3bf3c
Jack-Khuu <jack.khuu.7@gmail.com>
2026-02-13 11:42:48 -0800

    Missed changes

--- kernelboard/api/leaderboard.py
+++ kernelboard/api/leaderboard.py
@@ -1,4 +1,3 @@
-import re
 from typing import Any, List
 from flask import Blueprint
 from kernelboard.lib.db import get_db_connection
@@ -269,7 +268,7 @@ def get_custom_trend(leaderboard_id: int):
 
     total_time = (time.perf_counter() - total_start) * 1000
     logger.info(
-        "[Perf] timeseries leaderboard_id=%s | query=%.2fms | total=%.2fms",
+        "[Perf] custom_trend leaderboard_id=%s | query=%.2fms | total=%.2fms",
         leaderboard_id, query_time, total_time,
     )
 
